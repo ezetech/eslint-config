8d17960f02d9faba6b6a81ac18bbb363f98be84b
ihors <ihors91@gmail.com>
2022-05-19 16:27:41 +0300

    v1.1.0 - remove airbnb dependency

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    'airbnb',
     'plugin:security/recommended',
   ],
   plugins: [
